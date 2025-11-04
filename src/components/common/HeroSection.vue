5d42ac4faefbe8b7637916cee852cde92dc2dc59
Hadinata Jenta <70001259>
2025-11-04 16:32:19 +0700

    update: adjust padding in HeroSection for improved layout consistency

--- src/components/common/HeroSection.vue
+++ src/components/common/HeroSection.vue
@@ -9,7 +9,7 @@
             </div>
         </div>
 
-        <div class="relative max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-20 lg:py-32">
+        <div class="relative max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-18 lg:py-28">
             <div class="grid gap-12 lg:grid-cols-2 lg:gap-16 items-center">
                 <div class="space-y-8 text-left">
                     <div
